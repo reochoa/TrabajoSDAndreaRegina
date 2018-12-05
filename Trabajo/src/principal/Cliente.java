1ea6f4a19c0beac4b82ec9da3908f528df8a1daf
reochoa <regina.ochoa.esquiu@gmail.com>
2018-12-05 12:17:38 +0100

    Prueba2

--- Trabajo/src/principal/Cliente.java
+++ Trabajo/src/principal/Cliente.java
@@ -4,7 +4,7 @@ public class Cliente {
 	public static void main(String[] args) {
 		System.out.println("hola");
 		System.out.println("prueba");
-		System.out.println("prueba2");
+		System.out.println("prueba21");
 		
 	}
 
